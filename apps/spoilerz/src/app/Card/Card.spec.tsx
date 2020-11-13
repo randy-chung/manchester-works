e222b55700eb6456ef32a7af1b157083a508b993
Randy Chung <randy.chung@gmail.com>
2020-11-12 20:51:47 -0500

    Messing around with storybook, added a Card component with story.

---
+++ apps/spoilerz/src/app/Card/Card.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Card from './Card';
+
+describe('Card', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Card />);
+
+    expect(baseElement).toBeTruthy();
+  });
+});
