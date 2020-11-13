82f2bdc0e1eb5f1944017fa36c402816bcd30bd6
Randy Chung <randy.chung@gmail.com>
2020-11-13 16:02:59 -0500

    Added storybook controls plugin.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-knobs/register', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-essentials',
+    '@storybook/addon-controls',
+  ],
 };
