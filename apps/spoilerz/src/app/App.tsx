d907cca0b5dd23b6c9f62151b9a181fe100ab7d8
Randy Chung <randy.chung@gmail.com>
2020-11-14 16:22:39 -0500

    SPL-2 - Created a redux store for the app.

--- apps/spoilerz/src/app/App.tsx
+++ apps/spoilerz/src/app/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { Feed } from './Feed/Feed';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export class App extends React.PureComponent {
+/**
+ * Temporary -- we will create a real reducer in its own file later.
+ */
+function tmpReducer() {}
+
+export class App extends React.Component {
+  /** Global redux store for the app. */
+  private store = createStore(tmpReducer);
+
   constructor(props) {
     super(props);
+    this.state = {};
   }
 
   render() {
-    return <Feed />;
+    return (
+      <Provider store={this.store}>
+        <Feed />;
+      </Provider>
+    );
   }
 }
