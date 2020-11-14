948ea207a9481a33e6eb2b5c3c0eaaa173b291e7
Randy Chung <randy.chung@gmail.com>
2020-11-14 16:41:22 -0500

    SPL-2 - Moved all the store creation stuff to main.tsx, instead of being in the App component. Added middleware initialization.

--- apps/spoilerz/src/app/App.tsx
+++ apps/spoilerz/src/app/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import { Feed } from './Feed/Feed';
 
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-/**
- * Temporary -- we will create a real reducer in its own file later.
- */
-function tmpReducer() {}
-
 export class App extends React.Component {
-  /** Global redux store for the app. */
-  private store = createStore(tmpReducer);
-
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return (
-      <Provider store={this.store}>
-        <Feed />;
-      </Provider>
-    );
+    return <Feed />;
   }
 }
--- apps/spoilerz/src/main.tsx
+++ apps/spoilerz/src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, Store } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware, { SagaMiddleware } from 'redux-saga';
 
 import { App } from './app/App';
 
+/**
+ * Temporary -- we will create a real reducer in its own file later.
+ */
+function tmpReducer() {}
+
+// Initialize redux store and saga middleware.
+const sagaMiddleware: SagaMiddleware = createSagaMiddleware();
+const store: Store = createStore(tmpReducer, applyMiddleware(sagaMiddleware));
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
