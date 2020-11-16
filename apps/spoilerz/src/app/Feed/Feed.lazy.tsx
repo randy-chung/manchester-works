9fc669ec914f3f9a71c7946a935f805ce1c2dd06
Randy Chung <randy.chung@gmail.com>
2020-11-16 18:27:02 -0500

    SPL-2 - Commented out the lazy loading file for Feed component for now. I'll figure this out later.

--- apps/spoilerz/src/app/Feed/Feed.lazy.tsx
+++ apps/spoilerz/src/app/Feed/Feed.lazy.tsx
@@ -1,11 +1,11 @@
-import React, { lazy, Suspense } from 'react';
+// import React, { lazy, Suspense } from 'react';
 
-const LazyFeed = lazy(() => import('./Feed'));
+// const LazyFeed = lazy(() => import('./Feed'));
 
-const Feed = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
-  <Suspense fallback={null}>
-    <LazyFeed {...props} />
-  </Suspense>
-);
+// const Feed = (props: JSX.IntrinsicAttributes & { children?: React.ReactNode; }) => (
+//   <Suspense fallback={null}>
+//     <LazyFeed {...props} />
+//   </Suspense>
+// );
 
-export default Feed;
+// export default Feed;
