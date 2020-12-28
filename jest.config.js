7d3b85c8e774e4a1122ad13394c72fc4dfa08134
Randy Chung <randy.chung@gmail.com>
2020-12-28 13:03:51 -0500

    SPL-24, SPL-20 - Added a "server" Express app that we'll use to build out our API.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/spoilerz', '<rootDir>/apps/scraper-job'],
+  projects: [
+    '<rootDir>/apps/spoilerz',
+    '<rootDir>/apps/scraper-job',
+    '<rootDir>/apps/server',
+  ],
 };
